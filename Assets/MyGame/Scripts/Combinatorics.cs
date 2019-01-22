4e4c497e62f4c177add1d8b86f036ef9f0905db8
Sarah Glavan <s.glavan@sarahglavan-mac.local>
2019-01-22 08:05:57 +0100

    Assets add

---
+++ Assets/MyGame/Scripts/Combinatorics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Combinatorics
+{
+
+    private const int RANDOMSEED = 123;
+    //public static System.Random rnd = new System.Random(RANDOMSEED);
+    public static System.Random rnd = new System.Random(); //Random Object gebraucht
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count; //Größe der Liste den Wer n zuweisen
+        while (n > 1)
+        {
+            n--; 
+            int k = rnd.Next(n + 1); 
+            T value = list[k]; 
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
---
+++ Assets/MyGame/Scripts/Updater.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Updater : MonoBehaviour {
+
+    [SerializeField] Text text; // Variablen müssen nicht public sein
+    [SerializeField] string inputtext;
+    public GameObject circleColor;
+    // color nicht gebraucht
+    public GameObject[] shuffleCircles;
+
+    [SerializeField] GameObject circlePosition;
+
+    private List<Color> colorList;
+    private System.Random rnd = new System.Random();
+    
+    private void Start()
+    {
+       // input text soll nicht überschrieben werden
+
+        colorList = new List<Color> {
+            Color.red,
+            Color.blue,
+            Color.green, // comma vergessen
+            Color.magenta
+        };
+    }
+
+    public void UpdateTextWithInspectorInput()
+    {
+        text.text = inputtext; //Text muss text sein
+    }
+
+    public void UpdateCircleColor()
+    {
+        SpriteRenderer circleRenderer = circleColor.GetComponent<SpriteRenderer>(); 
+        byte r = (byte)rnd.Next(0, 255);
+        byte g = (byte)rnd.Next(0, 255);
+        byte b = (byte)rnd.Next(0, 255);
+        byte a = (byte)rnd.Next(0, 255); 
+
+        circleRenderer.color = new Color32(r, g, b, a);
+    }
+	
+    public void UpdateObjectPosition()
+    {
+        Transform circleTransform = circlePosition.GetComponent<Transform>();
+        
+        float randomPosX = UnityEngine.Random.Range(5, 433); //.Range statt .Next benutzen
+        circleTransform.localPosition = new Vector3(randomPosX, circleTransform.localPosition.y, circleTransform.localPosition.z);
+    }
+
+    public void ShuffleColorsInCircles()
+    {
+        List<SpriteRenderer> objectList = new List<SpriteRenderer>();
+        
+        foreach(GameObject item in shuffleCircles)
+        {
+            objectList.Add(item.GetComponent<SpriteRenderer>());
+        }
+
+        colorList.Shuffle();
+
+        for(int i = 0; i < objectList.Count; i++)
+        {
+            objectList[i].color = colorList[i];
+        }
+    }
+}
